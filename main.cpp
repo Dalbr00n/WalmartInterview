601dd147a2c0dac527d95ae852165d77bd561c77
Dalbr00n <64113688+Dalbr00n@users.noreply.github.com>
2021-03-25 02:12:15 -0500

    Updated main for input related to seating problem. Created foundation for seating algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include "main.hpp"
 
 int main(){
-    cout << system("Date") << endl;
+    uint8_t seat_open[10][20];
+    uint8_t row_full = 0;
+
+    ifstream input;
+    ofstream output;
+    input.open("input.txt");
+    output.open("output.txt");
+
+    string line;
+    string seats;
+    int nseats;
+    string reservation;
+    vector<int> seat;
+
+    if(input.is_open()){
+        while(getline(input, line)){
+            reservation = line.substr(0, line.find(' '));
+            seats = line.substr(line.find(' '));
+            nseats = stoi(seats);
+
+            for(row_full; row_full < 10; row_full++){
+                for(int i = 0; i < 20; i++){
+                    if(19 - nseats < i) break;
+                    else if(seat_open[row_full][i] == 0) continue;
+                    else{
+
+                    }
+                }
+            }
+            cout << reservation << endl;
+            seat.push_back(stoi(seats));
+        }
+    }
+    input.close();
+    int total = 0;
+    for(auto i = seat.begin(); i < seat.end(); i++){
+        cout << *i << ",";
+        total += *i;
+    }
+    cout << endl;
+    cout << "Total seats: " << total << endl;
     return 0;
 }
--- main.hpp
+++ main.hpp
@@ -5,6 +5,9 @@
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 
 
